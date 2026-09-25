6009cb09df98eb59148bfdad4c5fa12d213a5e5a
dhavanesiddhantlahu-tech <dhavanesiddhantlahu@gmail.com>
2026-09-25 09:38:00 +0530

    Unit 3 Concept 13: Virtual Destructor Safe Cleanup

---
+++ Program_37.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    virtual ~Base() { cout << "Base destructor executed" << endl; }
+};
+
+class Derived : public Base {
+public:
+    ~Derived() override { cout << "Derived destructor executed" << endl; }
+};
+
+int main() {
+    Base* ptr = new Derived();
+    delete ptr;
+    return 0;
+}
