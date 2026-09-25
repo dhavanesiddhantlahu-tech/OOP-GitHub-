e7445d9f8287807b16f10fec7bea0cc212ce57a8
dhavanesiddhantlahu-tech <dhavanesiddhantlahu@gmail.com>
2026-09-25 09:37:59 +0530

    Unit 3 Concept 8: Base Pointer Without Virtual Function (Static Binding)

---
+++ Program_32.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+class Base {
+public:
+    void display() const { cout << "Base display (Static Binding)" << endl; }
+};
+
+class Derived : public Base {
+public:
+    void display() const { cout << "Derived display (Direct Call)" << endl; }
+};
+
+int main() {
+    Derived d;
+    Base* ptr = &d;
+    ptr->display();
+    d.display();
+    return 0;
+}
