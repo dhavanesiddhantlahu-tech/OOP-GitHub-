7e80ab69c9b20f0d15b88d56a1554dbd1866d64c
dhavanesiddhantlahu-tech <dhavanesiddhantlahu@gmail.com>
2026-09-25 09:37:58 +0530

    Unit 3 Concept 3: Unary Minus Operator Overloading

---
+++ Program_27.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+class Balance {
+private:
+    double amount;
+public:
+    Balance(double amt = 0.0) : amount(amt) {}
+    Balance operator-() const { return Balance(-amount); }
+    void display() const { cout << "Balance: Rs. " << amount << endl; }
+};
+
+int main() {
+    Balance opening(5000.0);
+    Balance debt = -opening;
+    cout << "Original "; opening.display();
+    cout << "Negated "; debt.display();
+    return 0;
+}
