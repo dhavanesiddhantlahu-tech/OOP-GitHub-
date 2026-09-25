ecf0315448581bddb41af573814f3c37022e135f
dhavanesiddhantlahu-tech <dhavanesiddhantlahu@gmail.com>
2026-09-25 09:37:59 +0530

    Unit 3 Concept 9: Base Pointer With Virtual Function (Runtime Polymorphism)

---
+++ Program_33.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() const { cout << "Animal sound" << endl; }
+    virtual ~Animal() {}
+};
+
+class Dog : public Animal {
+public:
+    void sound() const override { cout << "Dog barks" << endl; }
+};
+
+class Cow : public Animal {
+public:
+    void sound() const override { cout << "Cow moos" << endl; }
+};
+
+int main() {
+    Dog d; Cow c;
+    Animal* a = &d;
+    a->sound();
+    a = &c;
+    a->sound();
+    return 0;
+}
