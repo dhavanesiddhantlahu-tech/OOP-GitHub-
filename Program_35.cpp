5cc5cd0505c7f2c33093ba10660382f2fad9ee82
dhavanesiddhantlahu-tech <dhavanesiddhantlahu@gmail.com>
2026-09-26 10:00:00 +0530

    Day 23: Program 35 - Abstract Classes and Pure Virtual Functions

---
+++ Program_35.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual double area() const = 0;
+    virtual ~Shape() {}
+};
+
+class Rectangle : public Shape {
+    double l, w;
+public:
+    Rectangle(double len, double wid) : l(len), w(wid) {}
+    double area() const override { return l * w; }
+};
+
+class Triangle : public Shape {
+    double b, h;
+public:
+    Triangle(double base, double height) : b(base), h(height) {}
+    double area() const override { return 0.5 * b * h; }
+};
+
+int main() {
+    Rectangle r(8.0, 4.0);
+    Triangle t(6.0, 5.0);
+    cout << "Rectangle Area: " << r.area() << endl;
+    cout << "Triangle Area: " << t.area() << endl;
+    return 0;
+}
